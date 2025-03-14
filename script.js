f0964f6cab53e6fca59953f224b5e8c39e0b371e
AiPromoCode <mrriaz.online@gmail.com>
2025-03-14 22:38:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+// Event listener to track clicks on the Sign Up button for analytics (optional)
+document.querySelector('button').addEventListener('click', function() {
+    console.log('Sign Up button clicked');
+});
